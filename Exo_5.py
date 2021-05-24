252f1d75a94dd01f37635d7ab8b79308696ce035
cedricgautier <75094434+cedricgautier@users.noreply.github.com>
2021-05-25 00:59:00 +0200

    Debugging de l'exercice 4 + du résultat du nombre de possibilité et ajout d'exercice 5 (ex 4 en récursive)

--- Exo_5.py
+++ Exo_5.py
@@ -1,4 +1,4 @@
-from itertools import product
+#Recursive
 
 def main():
     numberOfDice = int(input("To play the game, Please enter x number of dice(s) you desire to have but is below the number of 10. \nEnter the value of how many dice you want to toss here : "))
@@ -13,24 +13,21 @@ def main():
             possibilities = facesOfDice ** numberOfDice
             print("For the number of dice you have and faces you gace, there are ", possibilities)
             wantedValue = int(input("Please enter the number you want to calculate the numbber of possibilities you have for x amount of dice and faces here : "))
-            listeOfAllFaces = []
-            for i in range(1,facesOfDice+1):
-                listeOfAllFaces.append(i)
-
-            numberOfPossibilities = 0
-
-            if wantedValue >= numberOfDice*facesOfDice:
-                return False, print("False")
-
-            for i in product(listeOfAllFaces,repeat=numberOfDice):
-                if sum(i) == wantedValue:
-                    numberOfPossibilities+=1
-
-            print ("The number of possibilities are : ", numberOfPossibilities)
-        else: 
-            print("ERROR! The value you have typed is higher than 6")
-            return ValueError
+            
+            def Resolution(facesOfDice,numberOfDice, wantedValue):
+                table=[[0]*(wantedValue+1) for i in range(numberOfDice+1)] #Initialize all entries as 0
+            
+                for j in range(1,min(facesOfDice+1,wantedValue+1)): #Table entries for only one dice
+                    table[1][j]=1
+                        
+                    # Fill rest of the entries in table using recursive relation
+                    # i: number of dice, j: sum
+                for i in range(2,numberOfDice+1):
+                    for j in range(1,wantedValue+1):
+                        for k in range(1,min(facesOfDice+1,j)):
+                            table[i][j]+=table[i-1][j-k]
+                    
+                return table[-1][-1]
+            print(Resolution(facesOfDice,numberOfDice,wantedValue))
 
 main()
-
-    
